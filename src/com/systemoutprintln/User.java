4f52efb491ea8de8d94fbc6b17b3637d8113cd3e
G Rupesh <rupesh.goru@yahoo.com>
2026-08-07 06:12:42 +0530

    Add System.out.println

---
+++ src/com/systemoutprintln/User.java
@@ -0,0 +1,12 @@
+package com.systemoutprintln;
+
+public class User {
+    /*POST MORTEM OF System.out.println();*/
+
+    /*System is a class
+    * out is a static variable
+    * println() is a method.*/
+    static void main(String[] args) {
+        System.err.println("Hello World!");
+    }
+}
