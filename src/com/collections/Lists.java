5db6e896d3fed75eef529ca6a69ec921f0fbe052
G Rupesh <rupesh.goru@yahoo.com>
2026-08-19 21:58:36 +0530

    learn: add Java ArrayList examples

---
+++ src/com/collections/Lists.java
@@ -0,0 +1,17 @@
+package com.collections;
+
+import java.util.ArrayList;
+
+public class Lists {
+  public static void main(String[] args) {
+      ArrayList<String> names = new ArrayList<>();
+      names.add("Goru");
+      names.add("Rupesh");
+      names.add("Surya");
+      names.add("Pavan");
+      names.add("Kumar");
+
+      System.out.println(names);
+
+    }
+}
