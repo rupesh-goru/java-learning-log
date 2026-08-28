c42b82dc20221a2c1097115b80c8eed1f343cdb6
G Rupesh <rupesh.goru@yahoo.com>
2026-08-28 18:21:41 +0530

    updated/extended/learn: lambda expression

--- src/com/java8/lambdaexpression/EmployeeTest.java
+++ src/com/java8/lambdaexpression/EmployeeTest.java
@@ -15,7 +15,10 @@ public class EmployeeTest {
         empList.add(new Employee(10, "Fayaz", 50005));
 
         //Collections.sort(empList, new EmployeeComparator());
-        Collections.sort(empList, (emp1, emp2) -> (int) (emp1.salary - emp2.salary));
+        Collections.sort(empList, (emp1, emp2) -> (int) (emp1.salary - emp2.salary));// sorting of salary in ascending order
+        System.out.println(empList);
+
+        Collections.sort(empList,(emp1, emp2) -> emp2.empId - emp1.empId ); // sorting of id in descending order.
         System.out.println(empList);
 
 
