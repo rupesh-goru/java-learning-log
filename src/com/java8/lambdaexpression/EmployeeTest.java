4291bfa3a368b3349daca5d2ff40b02b307c0b13
G Rupesh <rupesh.goru@yahoo.com>
2026-08-29 20:55:09 +0530

    lambda ex

--- src/com/java8/lambdaexpression/EmployeeTest.java
+++ src/com/java8/lambdaexpression/EmployeeTest.java
@@ -9,7 +9,7 @@ public class EmployeeTest {
     static void main(String[] args) {
 
         List<Employee> empList = new ArrayList<>();
-        empList.add(new Employee(7, "Sahithi", 64000));
+        empList.add(new Employee(7, "Sahithi", 94000));
         empList.add(new Employee(8, "Rukmini", 70000));
         empList.add(new Employee(9, "Kaira", 80000));
         empList.add(new Employee(10, "Fayaz", 50005));
