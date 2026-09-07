77003c3c7e8629a32008902634b1b68cbb9edbc3
G Rupesh <rupesh.goru@yahoo.com>
2026-09-07 21:56:42 +0530

    extended time ops

--- src/com/java8/localdate/TimeOperations.java
+++ src/com/java8/localdate/TimeOperations.java
@@ -88,5 +88,6 @@ YYYY gives full year
         System.out.println(LocalDateTime.now().minusHours(23));
         System.out.println(LocalDateTime.now().minusMinutes(28));
         System.out.println(LocalDateTime.now().minusYears(2));
+        System.out.println(LocalDateTime.now().minusMonths(2));
     }
 }
