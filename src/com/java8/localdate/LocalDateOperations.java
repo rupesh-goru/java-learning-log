8099b8366b698c2a8bec1142b06ef7443822c805
G Rupesh <rupesh.goru@yahoo.com>
2026-09-11 20:42:21 +0530

    local date ops exe

--- src/com/java8/localdate/LocalDateOperations.java
+++ src/com/java8/localdate/LocalDateOperations.java
@@ -40,6 +40,7 @@ public class LocalDateOperations {
         System.out.println(date.plusMonths(96));
         System.out.println(date.minusMonths(8));
         System.out.println(date.minusDays(1));
+        System.out.println(date.plusMonths(1));
 
         //50th of this year.
         LocalDate date1 = LocalDate.of(2000,1,1); //leap year
